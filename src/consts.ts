f74c6c05dba50cda7e8798f6bf58d5cedcc446a6
Andrés Valle <nellsavedra@gmail.com>
2023-04-16 10:02:50 +0200

    Push changes into HEAD | 27 files changed, 773 insertions(+)

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Andrés Valle — Front-End Engineer';
+export const SITE_DESCRIPTION = '';
+export const Site = {
+    title: "Andrés Valle — Front-End Engineer",
+    description: "",
+    header: ""
+}
